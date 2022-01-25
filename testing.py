be37b85075163352fafe5e97b0fbbc52dc0844eb
Alexander Samokhin <samokhin@shockland.ru>
2022-01-25 09:53:07 +0300

    fake agent fixed

--- testing.py
+++ testing.py
@@ -4,11 +4,12 @@ from fake_useragent import UserAgent, FakeUserAgentError
 
 try:
     ua = UserAgent(fallback='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:21.0) Gecko/20100101 Firefox/21.0')
+    agent = ua.random
 except FakeUserAgentError:
-    pass
+    print('Bebe1')
     
 response = requests.get("https://www.makeready.ru/schedule/Chempionat-Sankt-Peterburga-po-prakticheskoj-strelbe-iz-ruzhya-17-11-2022-44281317012022/", 
-                        headers={'User-Agent': ua.random})
+                        headers={'User-Agent': agent})
 if response.status_code == 200:
     bs = BeautifulSoup(response.text, 'lxml')
     scheduler = bs.find('a', string='Брифинги')
